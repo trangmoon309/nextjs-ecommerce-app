d9d0fa10dca45038bc3f70490a9fdca1ea183546
Trang Huynh <trangnphuynh@kms-technology.com>
2025-05-24 12:49:42 +0700

    Config NextAuth

---
+++ auth.ts
@@ -0,0 +1,72 @@
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '@/db/prisma';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { compareSync } from 'bcrypt-ts-edge';
+import type { NextAuthConfig } from 'next-auth';
+
+export const config = {
+  pages: {
+    signIn: '/sign-in',
+    signOut: '/sign-out',
+    error: '/sign-in',
+    verifyRequest: '/auth/verify-request', // (used for check email message)
+    newUser: '/auth/new-user', // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { type: 'email' },
+        password: { type: 'password' },
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
+
+        // Find user in the database
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email as string,
+          },
+        });
+
+        // Check if user exists and if the password matches
+        if (user && user.password) {
+          const isMatch = compareSync(credentials.password as string, user.password);
+
+          // If password is correct, return user
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+
+        // If user does not exist or password does not match
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user, trigger, token }: any) {
+      // Set the userID from the token
+      session.user.id = token.sub;
+
+      // If there is an update, set the user name
+      if (trigger === 'update') {
+        session.user.name = user.name;
+      }
+
+      return session;
+    },
+  },
+} satisfies NextAuthConfig; // ensure the configuration object compatible with NextAuthConfig
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
--- db/seed.ts
+++ db/seed.ts
@@ -1,12 +1,20 @@
-import { PrismaClient } from '@/lib/generated/prisma';
+import { PrismaClient } from '@prisma/client';
 import sampleData from './sample-data';
 
 async function main() {
   const prisma = new PrismaClient();
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
 
   await prisma.product.createMany({
     data: sampleData.products,
+  });
+
+  await prisma.user.createMany({
+    data: sampleData.users,
   });
 
   console.log('Database seeded successfully!');
