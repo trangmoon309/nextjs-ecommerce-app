ac6ae69696ed314ba8626f1d6475289b69c71797
Trang Huynh <trangnphuynh@kms-technology.com>
2025-05-26 22:12:36 +0700

    Add cart tables

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,12 @@
 import { z } from 'zod';
-import { insertProductSchema } from '@/lib/validator';
+import { cartItemSchema, insertCartSchema, insertProductSchema } from '@/lib/validator';
 
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
   rating: number;
   createdAt: Date;
 };
+
+export type Cart = z.infer<typeof insertCartSchema>;
+
+export type CartItem = z.infer<typeof cartItemSchema>;
