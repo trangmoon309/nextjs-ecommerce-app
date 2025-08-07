c210157ab80278211c8880323aab03f49e452053
Trang Huynh <trangnphuynh@kms-technology.com>
2025-08-07 19:16:53 +0700

    Add banner carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
+import ProductCarousel from '@/components/shared/product/product-carousel';
 import ProductList from '@/components/shared/product/product-list';
-import { getLatestProducts } from '@/lib/actions/product.action';
+import { getFeaturedProducts, getLatestProducts } from '@/lib/actions/product.action';
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
   const normalized = latestProducts.map((product) => ({
     ...product,
@@ -12,6 +14,7 @@ const Homepage = async () => {
 
   return (
     <>
+      {featuredProducts.length > 0 && <ProductCarousel data={featuredProducts} />}
       <ProductList
         // data={sampleData.products}
         data={normalized}
--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -183,3 +183,16 @@ export async function getAllCategories() {
 
   return data;
 }
+
+// Get featured products
+export async function getFeaturedProducts() {
+  const data = await prisma.product.findMany({
+    where: {
+      isFeatured: true,
+    },
+    orderBy: { createdAt: 'desc' },
+    take: 4,
+  });
+
+  return convertToPlainObject(data);
+}
